f989df133e7ef26229279b148a40f1048a18c2a6
jruizj74 <jruizj74@gmail.com>
2021-02-21 14:09:07 +0100

    item2 reviewed

--- src/effectivejava/chapter2/item2/hierarchicalbuilder/Pizza.java
+++ src/effectivejava/chapter2/item2/hierarchicalbuilder/Pizza.java
@@ -1,28 +1,35 @@
 package effectivejava.chapter2.item2.hierarchicalbuilder;
-import java.util.*;
+
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
 
 // Builder pattern for class hierarchies (Page 14)
 
 // Note that the underlying "simulated self-type" idiom  allows for arbitrary fluid hierarchies, not just builders
 
 public abstract class Pizza {
-    public enum Topping { HAM, MUSHROOM, ONION, PEPPER, SAUSAGE }
-    final Set<Topping> toppings;
+	public enum Topping {
+		HAM, MUSHROOM, ONION, PEPPER, SAUSAGE
+	}
 
-    abstract static class Builder<T extends Builder<T>> {
-        EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
-        public T addTopping(Topping topping) {
-            toppings.add(Objects.requireNonNull(topping));
-            return self();
-        }
+	final Set<Topping> toppings;
 
-        abstract Pizza build();
+	abstract static class Builder<T extends Builder<T>> {
+		EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
 
-        // Subclasses must override this method to return "this"
-        protected abstract T self();
-    }
-    
-    Pizza(Builder<?> builder) {
-        toppings = builder.toppings.clone(); // See Item 50
-    }
+		public T addTopping(Topping topping) {
+			toppings.add(Objects.requireNonNull(topping));
+			return self();
+		}
+
+		abstract Pizza build();
+
+		// Subclasses must override this method to return "this"
+		protected abstract T self();
+	}
+
+	Pizza(Builder<?> builder) {
+		toppings = builder.toppings.clone(); // See Item 50
+	}
 }
--- src/effectivejava/chapter2/item2/hierarchicalbuilder/PizzaTest.java
+++ src/effectivejava/chapter2/item2/hierarchicalbuilder/PizzaTest.java
@@ -1,17 +1,17 @@
 package effectivejava.chapter2.item2.hierarchicalbuilder;
 
-import static effectivejava.chapter2.item2.hierarchicalbuilder.Pizza.Topping.*;
-import static effectivejava.chapter2.item2.hierarchicalbuilder.NyPizza.Size.*;
+import static effectivejava.chapter2.item2.hierarchicalbuilder.NyPizza.Size.SMALL;
+import static effectivejava.chapter2.item2.hierarchicalbuilder.Pizza.Topping.HAM;
+import static effectivejava.chapter2.item2.hierarchicalbuilder.Pizza.Topping.ONION;
+import static effectivejava.chapter2.item2.hierarchicalbuilder.Pizza.Topping.SAUSAGE;
 
 // Using the hierarchical builder (Page 16)
 public class PizzaTest {
-    public static void main(String[] args) {
-        NyPizza pizza = new NyPizza.Builder(SMALL)
-                .addTopping(SAUSAGE).addTopping(ONION).build();
-        Calzone calzone = new Calzone.Builder()
-                .addTopping(HAM).sauceInside().build();
-        
-        System.out.println(pizza);
-        System.out.println(calzone);
-    }
+	public static void main(String[] args) {
+		NyPizza pizza = new NyPizza.Builder(SMALL).addTopping(SAUSAGE).addTopping(ONION).build();
+		Calzone calzone = new Calzone.Builder().addTopping(HAM).sauceInside().build();
+
+		System.out.println(pizza);
+		System.out.println(calzone);
+	}
 }
