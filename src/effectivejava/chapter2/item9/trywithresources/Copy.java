85c1634e605fc43720c9070e2a8fa95d3d20a1ee
jruizj74 <jruizj74@gmail.com>
2021-03-05 20:30:01 +0100

    item9 reviewed

--- src/effectivejava/chapter2/item9/trywithresources/Copy.java
+++ src/effectivejava/chapter2/item9/trywithresources/Copy.java
@@ -1,24 +1,27 @@
 package effectivejava.chapter2.item9.trywithresources;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class Copy {
-    private static final int BUFFER_SIZE = 8 * 1024;
+	private static final int BUFFER_SIZE = 8 * 1024;
 
-    // try-with-resources on multiple resources - short and sweet (Page 35)
-    static void copy(String src, String dst) throws IOException {
-        try (InputStream   in = new FileInputStream(src);
-             OutputStream out = new FileOutputStream(dst)) {
-            byte[] buf = new byte[BUFFER_SIZE];
-            int n;
-            while ((n = in.read(buf)) >= 0)
-                out.write(buf, 0, n);
-        }
-    }
+	// try-with-resources on multiple resources - short and sweet (Page 35)
+	static void copy(String src, String dst) throws IOException {
+		try (InputStream in = new FileInputStream(src); OutputStream out = new FileOutputStream(dst)) {
+			byte[] buf = new byte[BUFFER_SIZE];
+			int n;
+			while ((n = in.read(buf)) >= 0)
+				out.write(buf, 0, n);
+		}
+	}
 
-    public static void main(String[] args) throws IOException {
-        String src = args[0];
-        String dst = args[1];
-        copy(src, dst);
-    }
+	public static void main(String[] args) throws IOException {
+		String src = args[0];
+		String dst = args[1];
+		copy(src, dst);
+	}
 }
