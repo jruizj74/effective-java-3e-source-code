c613c27cc40836c87a96655d974e0bdcffa8bf8e
jruizj74 <jruizj74@gmail.com>
2021-03-15 19:57:23 +0100

    item10 reviewed

--- src/effectivejava/chapter3/item10/CaseInsensitiveString.java
+++ src/effectivejava/chapter3/item10/CaseInsensitiveString.java
@@ -6,36 +6,39 @@ import java.util.Objects;
 
 // Broken - violates symmetry!  (Page 39)
 public final class CaseInsensitiveString {
-    private final String s;
+	private final String s;
 
-    public CaseInsensitiveString(String s) {
-        this.s = Objects.requireNonNull(s);
-    }
+	public CaseInsensitiveString(String s) {
+		this.s = Objects.requireNonNull(s);
+	}
 
-    // Broken - violates symmetry!
-    @Override public boolean equals(Object o) {
-        if (o instanceof CaseInsensitiveString)
-            return s.equalsIgnoreCase(
-                    ((CaseInsensitiveString) o).s);
-        if (o instanceof String)  // One-way interoperability!
-            return s.equalsIgnoreCase((String) o);
-        return false;
-    }
+	// Broken - violates symmetry!
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof CaseInsensitiveString)
+			return s.equalsIgnoreCase(((CaseInsensitiveString) o).s);
+		if (o instanceof String) // One-way interoperability!
+			return s.equalsIgnoreCase((String) o);
+		return false;
+	}
 
-    // Demonstration of the problem (Page 40)
-    public static void main(String[] args) {
-        CaseInsensitiveString cis = new CaseInsensitiveString("Polish");
-        String s = "polish";
+	// Demonstration of the problem (Page 40)
+	public static void main(String[] args) {
+		CaseInsensitiveString cis = new CaseInsensitiveString("Polish");
+		String s = "polish";
 
-        List<CaseInsensitiveString> list = new ArrayList<>();
-        list.add(cis);
+		List<CaseInsensitiveString> list = new ArrayList<>();
+		list.add(cis);
 
-        System.out.println(list.contains(s));
-    }
+		System.out.println(list.contains(s));
 
-//    // Fixed equals method (Page 40)
-//    @Override public boolean equals(Object o) {
-//        return o instanceof CaseInsensitiveString &&
-//                ((CaseInsensitiveString) o).s.equalsIgnoreCase(s);
-//    }
+		System.out.println(cis.equals(s));
+		System.out.println(s.equals(cis));
+	}
+
+	// // Fixed equals method (Page 40)
+	// @Override public boolean equals(Object o) {
+	// return o instanceof CaseInsensitiveString &&
+	// ((CaseInsensitiveString) o).s.equalsIgnoreCase(s);
+	// }
 }
