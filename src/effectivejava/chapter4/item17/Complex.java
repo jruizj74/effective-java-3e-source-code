2aced230d5f51e36d1ce973af21f9969c31672da
jruizj74 <jruizj74@gmail.com>
2021-03-23 18:43:27 +0100

    item17 reviewed

--- src/effectivejava/chapter4/item17/Complex.java
+++ src/effectivejava/chapter4/item17/Complex.java
@@ -2,61 +2,67 @@ package effectivejava.chapter4.item17;
 
 // Immutable complex number class (Pages 81-82)
 public final class Complex {
-    private final double re;
-    private final double im;
+	private final double re;
+	private final double im;
 
-    public static final Complex ZERO = new Complex(0, 0);
-    public static final Complex ONE  = new Complex(1, 0);
-    public static final Complex I    = new Complex(0, 1);
+	public static final Complex ZERO = new Complex(0, 0);
+	public static final Complex ONE = new Complex(1, 0);
+	public static final Complex I = new Complex(0, 1);
 
-    public Complex(double re, double im) {
-        this.re = re;
-        this.im = im;
-    }
+	public Complex(double re, double im) {
+		this.re = re;
+		this.im = im;
+	}
 
-    public double realPart()      { return re; }
-    public double imaginaryPart() { return im; }
+	public double realPart() {
+		return re;
+	}
 
-    public Complex plus(Complex c) {
-        return new Complex(re + c.re, im + c.im);
-    }
+	public double imaginaryPart() {
+		return im;
+	}
 
-    // Static factory, used in conjunction with private constructor (Page 85)
-    public static Complex valueOf(double re, double im) {
-        return new Complex(re, im);
-    }
+	public Complex plus(Complex c) {
+		return new Complex(re + c.re, im + c.im);
+	}
 
-    public Complex minus(Complex c) {
-        return new Complex(re - c.re, im - c.im);
-    }
+	// Static factory, used in conjunction with private constructor (Page 85)
+	public static Complex valueOf(double re, double im) {
+		return new Complex(re, im);
+	}
 
-    public Complex times(Complex c) {
-        return new Complex(re * c.re - im * c.im,
-                re * c.im + im * c.re);
-    }
+	public Complex minus(Complex c) {
+		return new Complex(re - c.re, im - c.im);
+	}
 
-    public Complex dividedBy(Complex c) {
-        double tmp = c.re * c.re + c.im * c.im;
-        return new Complex((re * c.re + im * c.im) / tmp,
-                (im * c.re - re * c.im) / tmp);
-    }
+	public Complex times(Complex c) {
+		return new Complex(re * c.re - im * c.im, re * c.im + im * c.re);
+	}
 
-    @Override public boolean equals(Object o) {
-        if (o == this)
-            return true;
-        if (!(o instanceof Complex))
-            return false;
-        Complex c = (Complex) o;
+	public Complex dividedBy(Complex c) {
+		double tmp = c.re * c.re + c.im * c.im;
+		return new Complex((re * c.re + im * c.im) / tmp, (im * c.re - re * c.im) / tmp);
+	}
 
-        // See page 47 to find out why we use compare instead of ==
-        return Double.compare(c.re, re) == 0
-                && Double.compare(c.im, im) == 0;
-    }
-    @Override public int hashCode() {
-        return 31 * Double.hashCode(re) + Double.hashCode(im);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (o == this)
+			return true;
+		if (!(o instanceof Complex))
+			return false;
+		Complex c = (Complex) o;
 
-    @Override public String toString() {
-        return "(" + re + " + " + im + "i)";
-    }
+		// See page 47 to find out why we use compare instead of ==
+		return Double.compare(c.re, re) == 0 && Double.compare(c.im, im) == 0;
+	}
+
+	@Override
+	public int hashCode() {
+		return 31 * Double.hashCode(re) + Double.hashCode(im);
+	}
+
+	@Override
+	public String toString() {
+		return "(" + re + " + " + im + "i)";
+	}
 }
