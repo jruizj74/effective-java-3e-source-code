a3dcf1fff2f137732f19f48e3314df2bb02aa4d3
jruizj74 <jruizj74@gmail.com>
2021-04-19 20:35:29 +0200

    item25 reviewed

--- src/effectivejava/chapter4/item25/Main.java
+++ src/effectivejava/chapter4/item25/Main.java
@@ -2,7 +2,7 @@ package effectivejava.chapter4.item25;
 
 //  (Page 115)
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(Utensil.NAME + Dessert.NAME);
-    }
+	public static void main(String[] args) {
+		System.out.println(Utensil.NAME + Dessert.NAME);
+	}
 }
